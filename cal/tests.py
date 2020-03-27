7c8e3e124b813ff092a1d81b422838446d4a8760
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-03-27 13:43:23 -0400

    start adding unit tests

--- cal/tests.py
+++ cal/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Event
+from .forms import EventForm
+from datetime import date
 
-# Create your tests here.
+class EventTestCase(TestCase):
+    def test_eventform_valid(self):
+        instance = Event.objects.create(title='some event', start_date=date(2020, 4, 1))
+        form = EventForm(instance=instance)
+        self.assertEqual(form.is_valid(), True)
