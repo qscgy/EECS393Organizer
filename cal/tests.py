839a7d9787430484f9b331f6438807a50d07da39
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-04-14 17:41:41 -0400

    add some unit tests on EventForm

--- cal/tests.py
+++ cal/tests.py
@@ -1,10 +1,24 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from .models import Event
 from .forms import EventForm
 from datetime import date
 
-class EventTestCase(TestCase):
-    def test_eventform_valid(self):
-        instance = Event.objects.create(title='some event', start_date=date(2020, 4, 1))
-        form = EventForm(instance=instance)
-        self.assertEqual(form.is_valid(), True)
+class EventFormTests(TestCase):
+    # Test if the start date field is required
+    def test_needs_start_date(self):
+        form_data = {'title':'something'}
+        form = EventForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    
+    # Test if the title field is required
+    def test_needs_title(self):
+        form_data = {'start_date':date(2020, 4, 17)}
+        form = EventForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    
+    # Test if anything but the start date and title are required
+    def test_optionals(self):
+        form_data = {'title':'something', 'start_date':date(2020, 4, 17)}
+        form = EventForm(data=form_data)
+        self.assertTrue(form.is_valid())
